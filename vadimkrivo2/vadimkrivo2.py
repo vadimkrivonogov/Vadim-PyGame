70a0020ddf66fa4d269bdb420ed6d8322d75ed45
vadimkrivonogov <120089193+vadimkrivonogov@users.noreply.github.com>
2023-04-11 15:40:58 +0300

    Update vadimkrivo2.py

--- vadimkrivo2/vadimkrivo2.py
+++ vadimkrivo2/vadimkrivo2.py
@@ -1,4 +1,4 @@
-﻿import pygame
+import pygame
 #активируем "pygame"
 pygame.init()
 #регулируеи размер окна
@@ -13,18 +13,24 @@ BLACK = (0, 0, 0)
 ORANGE = (255, 165, 0)
 #рисуем снеговика
 pygame.draw.circle(ekraani_pind, WHITE, (150, 125), 25) # голова
-pygame.draw.circle(ekraani_pind, WHITE, (150, 175), 35) # тело
+pygame.draw.circle(ekraani_pind, WHITE, (150, 175), 35) #шар1
 pygame.draw.circle(ekraani_pind, BLACK, (138, 118), 4) # левый глаз
 pygame.draw.circle(ekraani_pind, BLACK, (163, 118), 4) # правый глаз
 pygame.draw.polygon(ekraani_pind, ORANGE, [(150, 140), (152, 132), (148, 132)]) # нос
-pygame.draw.line(ekraani_pind, WHITE, (135, 155), (115, 165), 3) # левая рука
-pygame.draw.line(ekraani_pind, WHITE, (165, 155), (185, 165), 3) # правая рука
-pygame.draw.circle(ekraani_pind, WHITE, (142, 182), 5) # левая нога
-pygame.draw.circle(ekraani_pind, WHITE, (158, 182), 5) # правая нога
-pygame.draw.circle(ekraani_pind, WHITE, (150, 225), 45) # еще один круг снизу 
+pygame.draw.circle(ekraani_pind, WHITE, (142, 182), 5) #шар2
+pygame.draw.circle(ekraani_pind, WHITE, (150, 225), 45) #шар3
+#метла
+pygame.draw.aaline(ekraani_pind, (85,5,5), (215,115), (215,230), 50)
+black=[0,0,0]
+lopp_x=250
+lopp_y=250
+for i in range(50):
+    pygame.draw.line(ekraani_pind,black,(215,230),(lopp_x,lopp_y),1)
+    lopp_x-=5
+    lopp_y+=15
 #команда, чтобы вывести это на экран
 pygame.display.flip()
-#бесконечный цыкл, чтобы окно не закрвывалось сразу, после открытия 
+#бесконечный цикл, чтобы окно не закрывалось сразу, после открытия 
 while True:
     event=pygame.event.poll()
     if event.type==pygame.QUIT:
